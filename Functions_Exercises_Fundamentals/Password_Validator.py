0991cabe7e4337c5d658d3b4cfdc70c573c7eeda
Danail Donkov <danail.donkov5@gmail.com>
2025-10-05 22:09:18 +0300

    Update and rename Password_Validator to Password_Validator.py

--- Functions_Exercises_Fundamentals/Password_Validator.py
+++ Functions_Exercises_Fundamentals/Password_Validator.py
@@ -1,9 +1,11 @@
 def password_validator(some_password: str):
     list_with_error_messages = []
+    
     if len(some_password) not in range(6, 11):
         list_with_error_messages.append("Password must be between 6 and 10 characters")
     if not some_password.isalnum():
         list_with_error_messages.append("Password must consist only of letters and digits")
+    
     digits_counter = 0
     for current_character in some_password:
         if current_character.isdigit():
